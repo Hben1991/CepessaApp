7734d90134303342bab6e099221a08e4ad6ca588
Ben <ben@humnt.studio>
2026-02-03 22:01:43 +0200

    Initial Commit

---
+++ Cepessa/CepessaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CepessaApp.swift
+//  Cepessa
+//
+//  Created by Ben Hen on 03/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct CepessaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
